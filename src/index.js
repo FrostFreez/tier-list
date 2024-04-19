560057f7afde1dac0f2a4f7e70c5fec681c18b98
FrostFreez <jo.can.aguiar@gmail.com>
2024-04-18 22:06:18 -0700

    tirando test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home/Home';
 import Tier from './pages/tier/Tier';
-import Test from './pages/test/Test';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
@@ -14,7 +13,6 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route exact path="/tiers/:tierId" element={<Tier/>} /> 
-      <Route exact path="/test" element={<Test/>} /> 
     </Routes>
     </Router>
   </React.StrictMode>,
